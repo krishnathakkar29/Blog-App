1d33746aa3b546127099b89062346d863855673b
Krishna Thakkar <work.krishnat@gmail.com>
2024-02-26 00:14:28 +0530

    Adding components and RTE

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,6 @@
 import React, { useId } from "react";
 
+//user will pass a reference as well
 const Input = React.forwardRef(function Input(
   { label, type = "text", className = "", ...props },
   ref
@@ -13,11 +14,11 @@ const Input = React.forwardRef(function Input(
         </label>
       )}
       <input
-        type={type}
-        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+        type="text"
+        className={` px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
         ref={ref}
-        {...props}
         id={id}
+        {...props}
       />
     </div>
   );
